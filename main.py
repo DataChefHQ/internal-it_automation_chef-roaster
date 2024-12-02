a2dac2044595f9107532b13d4903b980a2987e80
Soheil Sheybani <soheil@datachef.co>
2024-12-02 21:17:25 +0330

    feat: index.html

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, jsonify
+import time
+import boto3
+import threading
+
+app = Flask(__name__)
+s3_client = boto3.client('s3')
+
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+
+    # Return a loading message while processing in the background
+    return jsonify({"status": "loading", "message": "Processing your request..."})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
