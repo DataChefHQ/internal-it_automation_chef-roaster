3952b4d7c7b27ccaad1af21be5ff159e0e657648
Soheil Sheybani <soheil@datachef.co>
2024-12-03 20:34:14 +0330

    adding sls yaml file

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify
 import boto3
+import awsgi
 
 app = Flask(__name__)
 s3_client = boto3.client('s3')
@@ -16,6 +17,10 @@ def submit():
 
     # Return a loading message while processing in the background
     return jsonify({"status": "loading", "message": "Processing your request..."})
+
+
+def lambda_handler(event, context):
+    return awsgi.response(app, event, context)
 
 
 if __name__ == "__main__":
