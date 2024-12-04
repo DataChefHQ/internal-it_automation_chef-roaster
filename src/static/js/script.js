01193f517c7ba7191f83d2074446e5f66d272ed1
Soheil Sheybani <soheil@datachef.co>
2024-12-04 14:35:14 +0330

    showing image in the UI

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -5,25 +5,22 @@ function submitForm() {
 
     // Get the prompt text
     var prompt = document.getElementById("prompt").value;
-    fetch('/submit', {
+    fetch('/find', {
         method: 'POST',
         body: JSON.stringify({ prompt: prompt }),
         headers: { 'Content-Type': 'application/json' }
-    }).then(response => response.json())
-    .then(data => {
-        if (data.status === "loading") {
-            setTimeout(() => {
-                document.getElementById("loading").style.display = 'none';
-                document.getElementById("spinner").style.display = 'none';
-                document.getElementById("image").src = "https://s3.amazonaws.com/your-bucket-name/generated_image.jpg";
-                document.getElementById("image").style.display = 'block';
-            }, 5000);
-        }
+    }).then(
+        response => response.json()
+    ).then(data => {
+        document.getElementById("loading").style.display = 'none';
+        document.getElementById("spinner").style.display = 'none';
+        document.getElementById("image").src = data.url;
+        document.getElementById("image").style.display = 'block';
     });
 }
 
 function changeText(button) {
-    button.textContent = "Let's roast 😈";
+    button.textContent = "Let's roast some chef 😈";
 }
 
 function resetText(button) {
